8fb87b624587c87cde0a5034018c785c8157fff3
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-20 15:50:05 +0500

    Update app.js
    body-parser added

--- express/app.js
+++ express/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const boddyparser = require('body-parser');
 const routes = require('../routes/coupons');
+const bodyParser = require('body-parser');
 
+app.use(boddyparser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(routes); 
 
 module.exports = app;
