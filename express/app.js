2b63045e70c59743dc1765d40701ec1b3ffad2d7
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-20 14:40:12 +0500

    Update app.js

--- express/app.js
+++ express/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const stripe = require('stripe')('sk_test_51H5of9KvUlSUjYzsS2bwvB2Q9Lh4BIQklNbnxRM9Hhar797P7xxsU8hlLefpa1nGovHSlqMBI5sezHVu62mwYMv300Dikb6y96');
+const routes = require('../routes/coupons');
 
-app.use((req,res,next) => {
-    console.log('running from express');
-}); 
+app.use(routes); 
 
 module.exports = app;
