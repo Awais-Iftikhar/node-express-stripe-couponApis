4e9a2c31b2dac2fddf961cac3b92caa2660ce325
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-20 14:46:36 +0500

    Update coupons.js

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/coupons');
 
-router.get('api/coupons' , (req,res,next) => {
-
-});
+router.get('/api/coupons', controller.getcoupons);
 
 module.exports = router;
