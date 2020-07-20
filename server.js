27c279cd0f9000201874417889a1f739a5251fd5
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-20 14:19:13 +0500

    Update server.js
    node express app setup

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
-
+const app = require('./express/app');
 const port = process.env.PORT || 8081;
+app.set('port' , port);
 
-const server = http.createServer((req,res,next) => {
-    console.log('req');
-
-})
- server.listen(port);
+const server = http.createServer(app);
+ server.listen(port,() => {
+     console.log('running from port' +port);
+ });
