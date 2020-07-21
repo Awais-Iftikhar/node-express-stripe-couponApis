b276e35c2adaef69bd5cf2cb61dea38619a756b8
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-21 20:42:15 +0500

    configure connection and create user model
    create user model and configure connection with mongo db

--- express/app.js
+++ express/app.js
@@ -3,6 +3,15 @@ const app = express();
 const boddyparser = require('body-parser');
 const routes = require('../routes/coupons');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://awais:ad4J1OqN7mGAQ7cd@cluster0-cwpaz.mongodb.net/manageusers?retryWrites=true&w=majority`,{ useUnifiedTopology: true ,useNewUrlParser: true})
+.then(() => {
+  console.log('connected');
+})
+.catch(() => {
+  console.log('conn failure');
+})
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin" , "*");
