52932a4fad7090dd1e70fb6eb4ad5fa1a2870e12
Awais-Iftikhar <awaissunny00@gmail.com>
2020-07-22 23:49:30 +0500

    Add jsonwebtoken package

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/coupons');
+const auth = require('../middleware/checkauth');
 
-router.get('/api/coupons', controller.getcoupons);
-router.post('/api/coupons', controller.addcoupons);
+router.get('/api/coupons' ,auth, controller.getcoupons);
+router.post('/api/coupons',auth, controller.addcoupons);
+router.get('/api/coupons/:couponid',auth, controller.getsinglecoupon);
 router.delete('/api/coupons/:id', controller.deletecoupon);
 
 module.exports = router;
